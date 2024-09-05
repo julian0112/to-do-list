e0c4d6d202d051211806970b9b67313780cfce4d
Julian <julian.lsanchez01@gmail.com>
2024-09-04 23:00:34 -0600

    Changes in the virtual enviroment after creatig the react client

--- tags/urls.py
+++ tags/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from tags import views 
 
@@ -9,4 +10,5 @@ router.register(r'tags', views.TagsView, 'tags')
 
 urlpatterns = [
     path("api/v1/", include(router.urls))
+    
 ]
